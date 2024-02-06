268697928e612be21051cb22c555e88982fe2ef9
Thomas-Fch <thomas.fachinetti@gmail.com>
2024-02-06 16:06:41 +0100

    side menu

--- components/side-menu/Index.tsx
+++ components/side-menu/Index.tsx
@@ -5,7 +5,6 @@ export const SideMenu: React.FC = () => {
     return (
         <div className=' fixed z-10 h-screen w-[30%] overflow-y-scroll bg-[#201f23] pt-24 transition-transform duration-300 ease-in-out'>
             <Title />
-            <Categories />
-        </div>
+            <Categories />        </div>
     );
 };
