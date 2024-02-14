72fb00af8ed90cd206566cceda3802550381ccf1
Thomas-Fch <thomas.fachinetti@gmail.com>
2024-02-14 16:26:22 +0100

    categories buttons

--- components/side-menu/categories/Card.tsx
+++ components/side-menu/categories/Card.tsx
@@ -1,40 +1,42 @@
+'use client';
+import useSearchAndFilter from '@/store/useSearchAndFilter';
 import Image from 'next/image';
-import { useState } from 'react';
 import { categories } from "../../../utils/categories";
 
-export const Card: React.FC = () => {
-    const [active, setActive] = useState(null);
+interface CardProps {
+    onClick: () => void;
+}
 
-    const handleSelected = (index: any) => {
-        setActive((prevIndex) => {
-            const newIndex = prevIndex === index ? null : index;
-            return newIndex;
-        });
-    };
+export const Card: React.FC<CardProps> = ({ onClick }) => {
+
+    const { active, handleCardClick } = useSearchAndFilter();
 
     return (
-        <div className='mt-4 flex justify-evenly'>
-            {categories.map((data: any, index: number) => (
-                <button
-                    type='button'
-                    className='flex cursor-pointer flex-col items-center gap-2'
-                    key={data.name}
-                    onClick={() => handleSelected(index)}
-                >
-                    <Image
-                        alt={data.alt}
-                        src={data.img}
-                        style={{
-                            width: '90%',
-                            height: 'auto',
-                            padding: '1.25rem',
-                            borderRadius: '10px',
-                            backgroundColor: active === index ? '#272727' : '#2C2C2C',
-                        }}
-                    />
-                    <p className='text-xs font-bold' style={{ color: active === index ? "#7D47AE" : "#FFFF" }}>{data.name}</p>
-                </button>
-            ))}
+        <div className='mt-4 flex justify-evenly' onClick={onClick}>
+            {categories.map((item) => {
+                const { name, img, alt } = item;
+                return (
+                    <button
+                        type='button'
+                        className='flex cursor-pointer flex-col items-center gap-2'
+                        key={name}
+                        onClick={() => handleCardClick(name)}
+                    >
+                        <Image
+                            alt={alt}
+                            src={img}
+                            style={{
+                                width: '90%',
+                                height: 'auto',
+                                padding: '1.25rem',
+                                borderRadius: '10px',
+                                backgroundColor: active === name ? '#272727' : '#2C2C2C',
+                            }}
+                        />
+                        <p className='text-xs font-bold' style={{ color: active === name ? "#7D47AE" : "#FFFF" }}>{name}</p>
+                    </button>
+                )
+            })}
         </div>
     );
 }
